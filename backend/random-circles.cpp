f2692b1ef0710364e752bac3b3b031679f9b5fcd
Rodolfo Picoreti <rodolfo.picoreti@gmail.com>
2017-09-10 12:52:22 -0300

    added rx, new path animation

--- backend/random-circles.cpp
+++ backend/random-circles.cpp
@@ -1,18 +1,20 @@
+#include <chrono>
+#include <cmath>
 #include <is/is.hpp>
 #include <is/packer.hpp>
 #include <random>
-#include <chrono>
 
-int main(int , char **) {
+int main(int, char **) {
   auto is = is::connect("amqp://localhost:5672");
 
   std::default_random_engine generator;
   std::uniform_real_distribution<double> distribution(0.0, 1.0);
 
-  for (;;) {
-    is.publish("circles", is::msgpack(std::make_tuple(
-                              distribution(generator), distribution(generator),
-                              distribution(generator))));
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+  for (int i = 0;; ++i) {
+    const double w = 0.1;
+    is.publish("circles",
+               is::msgpack(std::make_tuple(std::cos(i * w), std::sin(i * 2 * w),
+                                           distribution(generator))));
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
   }
 }
